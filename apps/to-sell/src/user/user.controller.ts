9cca23275593eca64e87e954c9e89f674862068d
the-az-dev <the.az.work@gmail.com>
2024-08-14 03:47:31 +0300

    Adding secure access to all user information

--- apps/to-sell/src/user/user.controller.ts
+++ apps/to-sell/src/user/user.controller.ts
@@ -1,19 +1,16 @@
-import {
-  Controller,
-  Delete,
-  Get,
-  Headers,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { User } from './entities/user.entity';
-import { AuthGuard } from '../auth/guards/auth.guard';
+import { Controller, Delete, Get, Headers, Req, UseGuards } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { User } from "./entities/user.entity";
+import { AuthGuard } from "../auth/guards/auth.guard";
+import { Roles } from "../users-role/decorators/users-role.decorator";
+import { Role } from "../users-role/constants/users-role.enum";
 
 @Controller('/api/user')
 export class UserController {
   constructor(private readonly userSevice: UserService) {}
 
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard)
   @Get('all')
   getUsers(@Req() req: Request) {
     return this.userSevice.getUsers(req);
